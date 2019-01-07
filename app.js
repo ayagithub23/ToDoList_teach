f29c12e96d28c8311f738955c243efd74c45c619
ayagithub23 <neptune32ac@gmail.com>
2019-01-07 21:34:58 +0800

    20190107 追加

--- app.js
+++ app.js
@@ -48,14 +48,13 @@
 // });
 
 // [2018-12-22] 01. 設一個物件，為了要印在HTML上。
-var todoData = [
-    {
-        id:uuid(),
+var todoData = [{
+        id: uuid(),
         content: "吃午餐",
         createdAt: 1
     },
     {
-        id:uuid(),
+        id: uuid(),
         content: "吃晚餐",
         createdAt: 1000
     }
@@ -65,51 +64,46 @@ var errorMsgData = "";
 
 
 console.log(todoData);
-function render(todoData,newTodoText,isInitial){
+
+function render(todoData, newTodoText, isInitial) {
     moment.locale('zh-tw');
-    // 驗證輸入的方式(包括什麼都沒輸入 以及重複輸入)
-    // if(!newTodoText) return;
-    // 傳進 var newTodoText 一樣
-    if(!isInitial){
-        var isValid = validate(newTodoText);
-    }else{
-        var isValid = true;
-    }
-
+    var $errorMsg = $("#errorMsg");
     var $addTodoInput = $("#addTodoInput");
-    if(isValid){
-        var HTML = "";
-        var $ul = $("ul");
-        // [2018-12-22] 04. 建立迴圈，因為寫成i<=todoData.length，導致找不到資料，會出問題
+    var HTML = "";
+    var $ul = $("ul");
+    // [2018-12-22] 04. 建立迴圈，因為寫成i<=todoData.length，導致找不到資料，會出問題
+    // [2018-12-22] app.js:32 Uncaught TypeError: Cannot read property 'content' of undefined at render
+    for (var i = 0; i < todoData.length; i++) {
+        // [2018-12-22] 如果輸入i變數的情況下
         // [2018-12-22] app.js:32 Uncaught TypeError: Cannot read property 'content' of undefined at render
-        for(var i = 0; i < todoData.length; i++){
-            // [2018-12-22] 如果輸入i變數的情況下
-            // [2018-12-22] app.js:32 Uncaught TypeError: Cannot read property 'content' of undefined at render
-            // [2018-12-22] 04. 其實我也可以這樣做
-            // $("ul").append(`<li><span class="delete"><button>刪除</button></span>${todoData[i].content} ${moment().format('llll')}</li>`);
-            // [2018-12-22] X錯誤 [2018-12-22] 04. 如果加了兩個點點"delete"按鈕樣式會不見。
-            HTML = HTML +
+        // [2018-12-22] 04. 其實我也可以這樣做
+        // $("ul").append(`<li><span class="delete"><button>刪除</button></span>${todoData[i].content} ${moment().format('llll')}</li>`);
+        // [2018-12-22] X錯誤 [2018-12-22] 04. 如果加了兩個點點"delete"按鈕樣式會不見。
+        HTML = HTML +
             `<li id="${todoData[i].id}"><span><button class="delete">刪除</button></span>
-
             ${todoData[i].content}
             ${moment().format('llll')}
             </li>`
-            // [2018-12-22] 不懂 CREATEAT ${moment(todoData[i].createdAt).format("MM/DD hh:mm")}
-        };
-        $("ul").empty();
-        $errorMsgData = "";
-        $ul.append(HTML);
-    }else{
-        var $errorMsg = $("#errorMsg");
-        $errorMsg.append(errorMsgData);
-    
-    }
-     $addTodoInput.val("");
-
-     $errorMsgData = "";
+        // [2018-12-22] 不懂 CREATEAT ${moment(todoData[i].createdAt).format("MM/DD hh:mm")}
+    };
+    $("ul").empty();
+    $errorMsgData = "";
+    $ul.append(HTML);
+    $addTodoInput.val("");
+    $errorMsg.text("");
 }
 
-function validate(newTodoText){
+function renderErr() {
+    var $errorMsg = $("#errorMsg");
+    var $addTodoInput = $("#addTodoInput");
+    $errorMsg.text(errorMsgData);
+    $errorMsgData = "";
+    $addTodoInput.val("");
+}
+
+
+
+function validate(newTodoText) {
     var isValid = true;
 
     // 驗證資料，若資料合法回傳true;
@@ -123,8 +117,9 @@ function validate(newTodoText){
     // }else{
     //     a = "錯誤";
     // }
-    if(!newTodoText){
+    if (!newTodoText) {
         errorMsgData = "請輸入內容";
+        isValid = false;
     }
 
 
@@ -133,44 +128,26 @@ function validate(newTodoText){
     // forEach 只能用在陣列上
     // forEach 無法中斷
     // todoData.forEach(function(eachTodo){
-        // if(eachTodo.content===newTodoText){
-        //     isValid = false;
-        // }
+    // if(eachTodo.content===newTodoText){
+    //     isValid = false;
+    // }
     // });
-    for(var i = 0 ; i<todoData.length;i++){
-        if(todoData[i].content ===  newTodoText){
+    for (var i = 0; i < todoData.length; i++) {
+        if (todoData[i].content === newTodoText) {
             isValid = false;
             errorMsgData = "重複輸入";
-            return;
         }
     }
-
-
-
     // 若資料不合法，回傳false
-
-
     return isValid;
-
-
 };
-// function renderErr(){
-//     // error message render 到 ui 上
-//     // var 我的 UI
-//     var $errorMsg = $("#errorMsg");
-//     var $addTodoInput = $("#addTodoInput");
-//     $addTodoInput.val("");
-//     $errorMsg.append(errorMsgData);
-//     errorMsgData = "";
-
-// }
 
 
-render(todoData,"",true);
+render(todoData, "", true);
 
 
 
-$("#addTodoBtn").on("click",function(){
+$("#addTodoBtn").on("click", function () {
     // alert("點我Button");
     // preventDefault() 方法阻止元素發生默認的行為（例如，當點擊提交按鈕時阻止對表單的提交）。
     // https://goo.gl/uAWuJS
@@ -189,27 +166,21 @@ $("#addTodoBtn").on("click",function(){
     moment.locale('zh-tw');
     // [2018-12-22] 05. 如果沒在 addTodoInput 輸入資料的話，那就不會出現LI，順序很重要，不能夠再APPEND下面，否則這功能會失效。
 
-    // if(!isValid){
-    //     renderErr();
-    //     return;
-    // }
-
-
-
-
-
-
-
-
     // [2018-12-22] 06. 使用者資料存進新的物件(建立新的物件)
+
+    var isValid = validate(newTodoText);
+    if (!isValid) {
+        renderErr();
+        return;
+    }
 
     var newTodoData = {
         // newTodoText 輸入得值
-        id:uuid(),
+        id: uuid(),
         content: newTodoText,
         createdAt: moment().valueOf()
     };
-     
+
     // [2018-12-22] 06. 如果我不想用push的方式，而是建立一支新物件，我該怎麼做。
     // var newTodoData = {
     //     content: newTodoText,
@@ -223,29 +194,22 @@ $("#addTodoBtn").on("click",function(){
 
     // [2018-12-22] 07. 新的物件陣列，push進todoData
     todoData.push(newTodoData);
-
-        
-
-
-    // [2018-12-22] 09. 執行函數，這樣才會在HTML上出現
-    render(todoData,newTodoText,false);
-    //  $("ul").append(`<li>${newTodoText} ${moment().format('llll')}</li>`);
-    // $addTodoInput.val("");
+    render(todoData);
 });
 // https://momentjs.com/
 // moment().format('MMMM Do YYYY, h:mm:ss a');
 
 
 // [2018-12-22] 03. 新增刪除按鈕 
-$("ul").on("click", ".delete", function(){
+$("ul").on("click", ".delete", function () {
     // $(this).parent("li").remove();
     // 先刪掉資料庫裡的資料
     var idToDelete = $(this).parents("li").attr("id");
     // alert(idToDelete);
     // 將匹配元素集合縮減為匹配選擇器或匹配函數返回值的新元素。
-        todoData = todoData.filter(function(todo){
-        if(todo.id === idToDelete) return false;
+    todoData = todoData.filter(function (todo) {
+        if (todo.id === idToDelete) return false;
         else return true;
     });
-    render(todoData,"",false);
+    render(todoData, "", false);
 });
